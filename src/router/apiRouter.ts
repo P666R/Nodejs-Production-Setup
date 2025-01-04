a6cbd46fb6608841aa033f65d2e6a01e44978dde
P666R <sam469roger@gmail.com>
2025-01-04 02:51:48 +0000

    feat: Implement health endpoint

--- src/router/apiRouter.ts
+++ src/router/apiRouter.ts
@@ -4,5 +4,6 @@ import apiController from '../controller/apiController';
 const router = express.Router();
 
 router.route('/self').get(apiController.self);
+router.route('/health').get(apiController.health);
 
 export default router;
